feb6860d116bf84afb97330e6eb9f7fe4aad7dc4
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-16 21:52:46 +0530

    Add files via upload

---
+++ Day1/Class Work/ex05/ex05.java
@@ -0,0 +1,15 @@
+class ex05{
+    public static void main (String[] args){
+        int Num1 = 10;
+        int Num2 = 19;
+
+
+        System.out.println("Addition Is " + (Num1+Num2));
+        System.out.println("Substraction Is " + (Num1-Num2));
+        System.out.println("Multiplication Is " + ((Num1*Num2)));
+        System.out.println("Division Is " + ((double)Num1/(double)Num2));
+        System.out.println("Modulas Is " + (Num1%Num2));
+
+
+    }
+}
