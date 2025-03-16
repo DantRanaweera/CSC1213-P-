0e6848c17a127e6975d3c276c50ff35b56d5e876
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-16 21:33:40 +0530

    Add files via upload

---
+++ Day1/Class Work/ex02/ex02.java
@@ -0,0 +1,24 @@
+class ex02{
+    public static void main (String[] args){
+        String fac = "FAS";
+        int n = 3;
+        double p =10.5;
+        int m = 5;
+        char w1 = '&';
+        boolean isTH = true;
+
+
+
+        System.out.println(fac);
+        System.out.println(n);
+        System.out.println(p);
+        System.out.println(m);
+        System.out.println(w1);
+        System.out.println(isTH);
+
+        System.out.println("My Faculty Is " + fac);
+        System.out.println("My Result_1 Is " + (n+m));
+        System.out.println("My Result_2 Is " + (p+m));
+
+    }
+}
