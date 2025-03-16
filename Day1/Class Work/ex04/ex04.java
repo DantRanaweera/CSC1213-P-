e57aa616e6ba623ae80d233afeb4dda436f91681
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-16 21:44:53 +0530

    Add files via upload

---
+++ Day1/Class Work/ex04/ex04.java
@@ -0,0 +1,19 @@
+class ex04{
+    public static void main (String[] args){
+        System.out.println("Name: Danthula Ranaweera");
+        System.out.println("Reg NO: 2022/ASP/03");
+        System.out.println("Degree Program: Applied Mathematics And Computing");
+
+        String Name = "Danthula Ranaweera";
+        String RegNo = "2022/ASP/03";
+        String Degree = "Applied Mathematics And Computing";
+
+        System.out.println("Full Name: " + Name);
+        System.out.println("Reg No: " + RegNo);
+        System.out.println("Degree Program: " + Degree);
+
+
+
+
+    }
+}
