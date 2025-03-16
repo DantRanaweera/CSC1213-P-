737f6bbf7d50cc291707120214bb3feef55a70e6
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-16 22:02:54 +0530

    Add files via upload

---
+++ Day1/Class Work/ex07/ex07.java
@@ -0,0 +1,20 @@
+class ex07{
+    public static void main (String[] args){
+        char a1 = 'k';
+        System.out.println("The Character Is: " + a1);
+
+        int int1 = (int)a1;
+        System.out.println("The Character Is: " + int1);
+
+        double b1 = 156.987;
+        System.out.println("The Value Is: " + b1);
+
+        int int2 = (int)b1;
+        System.out.println("The Value Is: " + int2);
+
+        double b2 = int1;
+        System.out.println("The Value Is: " + b2);
+
+
+    }
+}    
