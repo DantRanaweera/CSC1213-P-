a83b3ccf075848b045486072329bcf1a699def6a
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-16 21:58:09 +0530

    Add files via upload

---
+++ Day1/Class Work/ex06/ex06.java
@@ -0,0 +1,7 @@
+class ex06{
+    public static void main (String[] args){
+        System.out.println("Name: Danthula \nLast Name: Ranaweera \nAdress: Polhengoda Road,Colombo 05");
+        
+
+    }
+}
