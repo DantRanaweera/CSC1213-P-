1366fa87e7a2bd2cea9bf1ede18909ef43e7590c
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-22 21:08:43 +0530

    Add files via upload

---
+++ Day 2/Ex10/ex18.java
@@ -0,0 +1,16 @@
+public class ex18 {
+	
+	public static void main(String args[])
+	{
+		
+		int num = 7786;
+		
+		double dNum = (double) num;
+		
+		System.out.println("The value of "+num+" after converting to the double is "+dNum);
+		
+		int aftercon = (int) dNum;
+		
+		System.out.println("The value of "+dNum+" after converting to the int again is "+aftercon);
+	}
+}
