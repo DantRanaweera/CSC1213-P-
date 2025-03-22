e550dac77aa83999f4dc6881c61e1250624dd181
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-22 20:25:02 +0530

    Add files via upload

---
+++ Day 2/Ex06/ex14.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+class ex14{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System .in);
+
+        //Boolean
+        System.out.println("Enter a value: ");
+        boolean val1=sc.nextBoolean();
+        System.out.println("output: " + val1);
+
+        //Byte
+        System.out.println("Enter a value: ");
+        byte val2=sc.nextByte();
+        System.out.println("output: " +val2);
+
+        //Short
+        System.out.println("Enter a value: ");
+        short val3=sc.nextShort();
+        System.out.println("output: " +val3);
+
+        
+        //Integer
+        System.out.println("Enter a value: ");
+        int val4=sc.nextInt();
+        System.out.println("output: " +val4);
+
+        
+        //Double
+        System.out.println("Enter a value: ");
+        double val5=sc.nextDouble();
+        System.out.println("output: " + val5);
+
+        //Float
+        System.out.println("Enter a value: ");
+        float val6=sc.nextFloat();
+        System.out.println("output: "+val6);
+
+        //Long
+        System.out.println("Enter a value: ");
+        long val7=sc.nextLong();
+        System.out.println("output: "+val7);
+
+
+         
+         
+
+
+    }
+}   
