2502bf0a4481fd681fdc74a3dad54ff9c0515b54
DantRanaweera <danthulasandakaranaweera@gmail.com>
2025-03-22 21:00:27 +0530

    Add files via upload

---
+++ Day 2/Ex09/ex17.java
@@ -0,0 +1,13 @@
+//TypeCasting
+public class ex17{
+    
+    public static void main(String args[])
+    {
+     
+     int num1 = 3214;
+     double num2 = 11.7;
+     double sum = num1+num2;
+     
+     System.out.println("The sum of "+num1+" and "+num2+" is: "+sum);
+    }
+   }
